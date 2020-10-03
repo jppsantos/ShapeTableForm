ca49fcb6982022a509d2fdc9edc5660701e07536
Joao Paulo <joao.auhsey@gmail.com>
2020-10-03 12:05:18 -0300

    Change app initalization

---
+++ ShapeTableForm/ShapeTableForm/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShapeTableForm
+//
+//  Created by Joao Paulo Pereira dos Santos on 03/10/20.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
+
