36ceea4321ecc7ad82314cd34bef2360da067dd3
Joao Paulo <joao.auhsey@gmail.com>
2020-10-05 09:05:35 -0300

    Create Attribute struct and extension

---
+++ ShapeTableForm/ShapeTableForm/Model/Attribute.swift
@@ -0,0 +1,34 @@
+//
+//  Attribute.swift
+//  ShapeTableForm
+//
+//  Created by Joao Paulo Pereira dos Santos on 05/10/20.
+//
+
+import UIKit
+
+enum AttributeType {
+    case id
+    case name
+    case price
+    case doneIn
+}
+
+struct Attribute {
+    var type: AttributeType
+    var key: String
+    var value: String
+    var keyboardType: UIKeyboardType = .default
+}
+
+extension Attribute {
+    static func createEmptyFoodAttributeFields() -> [Attribute] {
+        let fields: [Attribute] = [
+            .init(type: .name, key: "Name", value: ""),
+            .init(type: .price, key: "Price", value: "", keyboardType: .numberPad),
+            .init(type: .doneIn, key: "Preparation Time", value: "", keyboardType: .numberPad)
+        ]
+        
+        return fields
+    }
+}
